a30944dce215415c48e9430e8fa783e45b96ae8d
Matheus Aeroso <matheusaeroso@gmail.com>
2018-09-08 17:45:47 -0300

    Incomplete implementation

--- src/me/maeroso/PeerManager.java
+++ src/me/maeroso/PeerManager.java
@@ -2,22 +2,18 @@ package me.maeroso;
 
 import me.maeroso.protocol.Peer;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 public class PeerManager {
     private static PeerManager ourInstance = new PeerManager();
+    KeyPair keyPair;
     private Peer ourPeer;
     private List<Peer> peerList;
 
@@ -34,8 +30,8 @@ public class PeerManager {
             int generatedPort = new Random().nextInt(50) + 4200;
             System.err.println("Generated instance port: " + generatedPort);
             try {
-                ourInstance.ourPeer = new Peer(new InetSocketAddress(InetAddress.getLocalHost(), generatedPort), keyPair.getPublic());
-            } catch (NoSuchAlgorithmException | UnknownHostException e) {
+                ourInstance.ourPeer = new Peer(new InetSocketAddress(InetAddress.getLocalHost(), generatedPort), getKeyPair().getPublic());
+            } catch (UnknownHostException e) {
                 e.printStackTrace();
             }
 
