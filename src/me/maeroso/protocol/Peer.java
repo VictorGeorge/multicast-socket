f985408bba72310b34482d61a1eced74d9a8cb97
Matheus Aeroso <matheusaeroso@gmail.com>
2018-09-09 00:54:03 -0300

    feat: Added greeting request/response functionality

--- src/me/maeroso/protocol/Peer.java
+++ src/me/maeroso/protocol/Peer.java
@@ -5,6 +5,7 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Peer implements Serializable {
@@ -17,7 +18,7 @@ public class Peer implements Serializable {
         this.socketAddress = socketAddress;
         this.publicKey = publicKey;
         this.privateKey = privateKey;
-        this.id = UUID.randomUUID().toString().substring(0,7);
+        this.id = UUID.randomUUID().toString().substring(0,4);
     }
 
     public InetAddress getAddress() {
@@ -34,6 +35,22 @@ public class Peer implements Serializable {
 
     public String getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Peer peer = (Peer) o;
+        return Objects.equals(id, peer.id) &&
+                Objects.equals(socketAddress, peer.socketAddress) &&
+                Objects.equals(publicKey, peer.publicKey) &&
+                Objects.equals(privateKey, peer.privateKey);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, socketAddress, publicKey, privateKey);
     }
 
     @Override
